12e42c0c9df0e70414a54d637e9de004cb371467
Ruben Anders <RAnders00@users.noreply.github.com>
2015-03-03 14:41:34 +0100

    Reference class.

---
+++ src/main/java/gmail/Lance5057/com/Reference.java
@@ -0,0 +1,10 @@
+package gmail.Lance5057.com;
+
+public class Reference
+{
+	
+	public static final String MOD_ID = "tinkersdefense";
+	public static final String VERSION = "1.1";
+	public static final String MOD_NAME = "Tinkers' Defense";
+	
+}
